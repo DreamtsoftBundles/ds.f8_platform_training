8af039a24e5cd99cd4e0b7de9aa00158e55d4725
Bow Ruggeri <bow@dreamtsoft.com>
2017-04-11 16:40:49 -0700

    Adding MyFullComponentClient example

---
+++ components/my_full_component/MyFullComponentServer.js
@@ -0,0 +1,21 @@
+var PageComponent = require("ds.base/PageComponent");
+
+var MyFullComponentServer = PageComponent.create({
+	data: function(attributes, vars) {
+		var message;
+
+		if (Object.isTrue(attributes.show_header)) {
+			message = "Hello with header";
+		} else {
+			message = "Hello without header";
+		}
+
+                return new StatusResponse('good', {
+                        serverMessage: message
+                });
+	},
+	
+	type: "MyFullComponentServer"
+});
+
+module.exports = MyFullComponentServer;
