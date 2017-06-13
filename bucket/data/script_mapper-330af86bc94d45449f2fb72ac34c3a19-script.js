22aff2a39b53a7e489187e0f8666c9a1eae59f5f
Jerrod Bennett <jerrod@dreamtsoft.com>
2017-06-13 11:50:25 -0700

    Commit by: System Administrator

--- bucket/data/script_mapper-330af86bc94d45449f2fb72ac34c3a19-script.js
+++ bucket/data/script_mapper-330af86bc94d45449f2fb72ac34c3a19-script.js
@@ -4,58 +4,74 @@
 var HTTPScriptable = require("core/HTTPScriptable");
 
 var GoogleSpreadsheetMapper = BaseMapper.create({
-    search: function(search) {
-        var h = new HTTPScriptable("https://docs.google.com/");
-        //h.setAuth(this.bucketAttributes.username, this.bucketAttributes.password);
+	search: function(search) {
+		var h = new HTTPScriptable("https://docs.google.com/");
+		//h.setAuth(this.bucketAttributes.username, this.bucketAttributes.password);
 
-        // the table here should be the key of the spreadsheet, from the "Publish to the web" dialog, in the URL:
-        // https://docs.google.com/spreadsheets/d/19C4BZaGZOl07kqRoMj8FEnuyfXdKD6yrfjopAioxxW4/pub?output=csv
-        // to create a new bucket backed by a spreadsheet:
-        // 1. publish the sheet to the web, copy the key to the table attribute of the new bucket
-        // 2. make sure the first row of the sheet matches the names of the slots you create, 
-        //     case does not matter and spaces from the sheet will be converted to _ to match the slotname
-        var path = "/spreadsheets/d/" + this.bucketAttributes.table + "/pub?output=csv";
-        var csvResponse = h.getString(path);
+		// the table here should be the key of the spreadsheet, from the "Publish to the web" dialog, in the URL:
+		// https://docs.google.com/spreadsheets/d/19C4BZaGZOl07kqRoMj8FEnuyfXdKD6yrfjopAioxxW4/pub?output=csv
+		// to create a new bucket backed by a spreadsheet:
+		// 1. publish the sheet to the web, copy the key to the table attribute of the new bucket
+		// 2. make sure the first row of the sheet matches the names of the slots you create, 
+		//     case does not matter and spaces from the sheet will be converted to _ to match the slotname
+		var path = "/spreadsheets/d/" + this.bucketAttributes.table + "/pub?output=csv";
+		var csvResponse = h.getString(path);
 
-        var records = [];
+		var records = [];
 
-        var lines = csvResponse.split("\n");
+		var lines = csvResponse.split("\n");
 
-        var headers = [];
-        var headerLine = lines[0].split(",");
-        for (var i = 0; i < headerLine.length; i++)
-            headers[i] = this._cleanQuotes(headerLine[i]).toLowerCase().replace(/\ /g, "_");
+		var headers = [];
+		var headerLine = lines[0].split(",");
+		for (var i = 0; i < headerLine.length; i++)
+			headers[i] = this._cleanQuotes(headerLine[i]).toLowerCase().replace(/\ /g, "_");
 
-        for (var j = 1; j < lines.length; j++) {
-            if (lines[j] === "")
-               continue;
-            var thisLine = lines[j].split(",");
+		for (var j = 1; j < lines.length; j++) {
+			if (lines[j] === "")
+				continue;
+			var thisLine = lines[j].split(",");
 
-            var thisRecord = {};
+			var thisRecord = {};
 
-            for (var k = 0; k < thisLine.length; k++) {
-                thisRecord[headers[k]] = thisLine[k];
-            }
-					 //thisRecord.id = record.item_id;
-                records.push(thisRecord);
-        }
-          console.log("GoogleSpreadsheetResponse: " + JSON.stringify(records));
-       //this.setNeedSort(true);
-       //this.setNeedSearch(true);
-        return records;
-    },
+			for (var k = 0; k < thisLine.length; k++) {
+				thisRecord[headers[k]] = thisLine[k];
+			}
+			//thisRecord.id = record.item_id;
+			records.push(thisRecord);
+		}
+		console.log("GoogleSpreadsheetResponse: " + JSON.stringify(records));
+		//this.setNeedSort(true);
+		//this.setNeedSearch(true);
+		return records;
+	},
 
-    _cleanQuotes: function(value) {
-        if (value.endsWith('"') && value.startsWith('"'))
-            return value.substring(1, value.length - 1);
+	_cleanQuotes: function(value) {
+		if (value.endsWith('"') && value.startsWith('"'))
+			return value.substring(1, value.length - 1);
 
-        return value;
-    },
+		return value;
+	},
 
-    insert: function(vals) {
-        console.log(JSON.stringify(vals));
-        return null;
-    }
+	insert: function(vals) {
+		console.log(JSON.stringify(vals));
+		return null;
+	},
+
+	api: function(name, data) {
+		console.log('API CALLED - ' + name);
+		if (name == 'get_url') {
+			var url = "https://docs.google.com/spreadsheets/d/" + this.bucketAttributes.table + "/pub?output=csv";
+			var returnObject = [{
+				'url': url
+			}];
+
+			console.log('URL FOUND - ' + url);
+			for (var key in returnObject) {
+				console.log("MAPPER OBJECT - " + key + " - " + returnObject[key]);
+			}
+			return returnObject;
+		}
+	},
 });
 
 module.exports = GoogleSpreadsheetMapper;
