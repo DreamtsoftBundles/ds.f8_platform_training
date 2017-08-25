df072125ee431f8b5874f889deeea50c7f8ea34f
Bow Ruggeri <bow@dreamtsoft.com>
2017-08-25 12:35:05 -0700

    Have angular example use server data as an example too

--- components/angular_example/AngularExampleServer.js
+++ components/angular_example/AngularExampleServer.js
@@ -2,14 +2,8 @@ var PageComponent = require("ds.base/PageComponent");
 
 var AngularExampleServer = PageComponent.create({
 	data: function(attributes, vars) {
-                var helloMessage = "Hello my angular";
-
-                if (Object.isTrue(attributes.specify_message)) {
-                        helloMessage = attributes.hello_message;
-                }
-
                 return new StatusResponse('good', {
-                        message: helloMessage
+                        my_data: "This is my data"
                 });
 	},
 	
